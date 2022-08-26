654ae1012ff11b04e666b68de7a17bb89204b952
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-26 11:59:57 +0100

    created interface class

---
+++ accountInterface.js
@@ -0,0 +1,24 @@
+class AccountInterface {
+  constructor(account, formatter) {
+    this.account = account;
+    this.formatter = formatter;
+  }
+
+  credit(amount) {
+    this.account.deposit(amount);
+  }
+
+  debit(amount) {
+    this.account.withdraw(amount);
+  }
+
+  getBalance() {
+    return `Your balance is now ${this.account.balance}`;
+  }
+
+  printStatement() {
+    return this.formatter.printStatement(this.account.transactions);
+  }
+}
+
+module.exports = AccountInterface;
